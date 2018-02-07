86dc16a605152f29245b1605666bc338ff7aea9c
Heloise-fernandes <heloise.fernandesdealmeida@gmail.com>
2018-02-07 16:03:27 +0100

    Ajout Icones + Début rooting

--- smartRecruiting/src/app/app.module.ts
+++ smartRecruiting/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AddOfferComponent } from './add-offer/add-offer.component';
+import { MainComponent } from './main/main.component';
 
+const appRoutes: Routes = [
+  { path: 'offre', component: AddOfferComponent },
+  { path: 'offre/:id', component: AddOfferComponent },
+  { path: '', component: MainComponent },
+  { path: '**', component: MainComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AddOfferComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes,{ enableTracing: true }),
   ],
   providers: [],
   bootstrap: [AppComponent]
