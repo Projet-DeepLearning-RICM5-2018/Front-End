6b57593184e943f99e3148f8ea84d348ce57cf64
Alicia Abonnenc <alicia.abonnenc@hotmail.com>
2018-02-08 09:05:51 +0100

    Ajout de la page mes offres

--- smartRecruiting/src/app/app.module.ts
+++ smartRecruiting/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AddOfferComponent } from './add-offer/add-offer.component';
 import { MainComponent } from './main/main.component';
+import { MyOffersComponent } from './my-offers/my-offers.component';
 
 const appRoutes: Routes = [
   { path: 'offre', component: AddOfferComponent },
   { path: 'offre/:id', component: AddOfferComponent },
+  { path: 'mes-offres', component: MyOffersComponent },
   { path: '', component: MainComponent },
   { path: '**', component: MainComponent }
 ];
@@ -19,7 +21,8 @@ const appRoutes: Routes = [
     AppComponent,
     HeaderComponent,
     AddOfferComponent,
-    MainComponent
+    MainComponent,
+    MyOffersComponent
   ],
   imports: [
     BrowserModule,
