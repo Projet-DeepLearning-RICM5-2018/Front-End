e56344a15e8507f8347dca673f82eb7e7ca6e52b
Alicia Abonnenc <alicia.abonnenc@hotmail.com>
2018-02-08 11:30:13 +0100

    Modification de la navbar en sticky

--- smartRecruiting/src/app/header/header.component.ts
+++ smartRecruiting/src/app/header/header.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  public connected : boolean;
+
   constructor() { }
 
   ngOnInit() {
+    this.connected = false;
+  }
+
+  connect() {
+    this.connected = true;
+  }
+
+  disconnect() {
+    this.connected = false;
   }
 
 }
