f21e62050bcd867b229c979ce3c41628839610d3
Alicia Abonnenc <alicia.abonnenc@hotmail.com>
2018-02-08 11:49:28 +0100

    Mes-offres

--- smartRecruiting/src/app/my-offers/my-offers.component.ts
+++ smartRecruiting/src/app/my-offers/my-offers.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-offers.component.css']
 })
 export class MyOffersComponent implements OnInit {
+  TestFormations = [
+    { id: 1, name: 'Cras justo odio' },
+    { id: 2, name: 'Dapibus ac facilisis in' },
+    { id: 3, name: 'Morbi leo risus' },
+  ];
+
+  TestRes = [
+    { id: 1, name: 'Cras justo odio' },
+    { id: 2, name: 'Dapibus ac facilisis in' },
+    { id: 3, name: 'Morbi leo risus' },
+  ];
+
+  resultats = [];
+  formations = [];
+  displayResults=false;
+
+  searchFormation(): void {
+    this.displayResults = true;
+    this.formations= this.TestFormations;
+  }
 
   constructor() { }
 
   ngOnInit() {
+    this.resultats = this.TestRes;
   }
 
 }
