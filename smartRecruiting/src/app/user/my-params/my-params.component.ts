f32ad2d8c1691b6d4ebe49bb08b4acae1c308297
Heloise-fernandes <heloise.fernandesdealmeida@gmail.com>
2018-03-09 09:32:34 +0100

    Revert "Revert "Correction prediction""
    This reverts commit e7d6e5250a00a017b3503b93bd0bdec8ad3e6235.

--- smartRecruiting/src/app/user/my-params/my-params.component.ts
+++ smartRecruiting/src/app/user/my-params/my-params.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthentificationService } from '../../services/authentification.service';
+import { UserService } from '../../services/user.service';
 @Component({
   selector: 'app-my-params',
   templateUrl: './my-params.component.html',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyParamsComponent implements OnInit {
 
-  constructor() { }
+  public userAccount :any;
+  public modyfing : boolean = false;
+
+  constructor(
+    private _auth : AuthentificationService,
+    private _userService : UserService,
+  ) { }
 
   ngOnInit() {
+    //{id: number;name: string;surname : string;role : string;email:string;password : string;isAdmin : boolean;}
+    this.userAccount = this._auth.getConnectedUser().value;
+    console.log(this.userAccount);
   }
+
+
 
 }
